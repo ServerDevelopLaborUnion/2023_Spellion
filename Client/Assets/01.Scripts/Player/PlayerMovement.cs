5fc9395b52000023a22d116a1d2f76f846b0b930
SungJieundev <sungjieun.dev@gmail.com>
2023-04-27 18:45:11 +0900

    gravityCalc

--- Client/Assets/01.Scripts/Player/PlayerMovement.cs
+++ Client/Assets/01.Scripts/Player/PlayerMovement.cs
@@ -9,10 +9,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private PlayerPropertySO _playerPropertySO;
     
+    private float _gravity = -9.8f;
     private CharacterController _characterController;
     private PlayerInput _playerInput;
 
     private Vector3 _movementVelocity;
+    private float _verticalVelocity;
 
     public bool CanMove = true;
 
@@ -29,8 +31,6 @@ public class PlayerMovement : MonoBehaviour
     private void CalcAcceleration(Vector3 _inputVelocity)
     {
         _inputVelocity.Normalize();
-
-        
         _movementVelocity = _inputVelocity * (_playerPropertySO.MoveSpeed * Time.fixedDeltaTime);
         
         // if(_movementVelocity.sqrMagnitude > 0)
@@ -48,14 +48,21 @@ public class PlayerMovement : MonoBehaviour
     {
         if (CanMove)
         {
-            _characterController.Move(_movementVelocity);
+            Vector3 move = _movementVelocity + _verticalVelocity * Vector3.up;
+            _characterController.Move(move);
+            //_characterController.Move(_movementVelocity);
+        }
+        
+        if(_characterController.isGrounded == false)
+        {
+            _verticalVelocity = _gravity * Time.fixedDeltaTime;
+        }
+        else
+        {
+            _verticalVelocity = _gravity * 0.3f * Time.fixedDeltaTime;
         }
     }
-
-    private void Move(Vector3 _directionInput)  
-    {
-        
-    }
+    
 
     private void Jump()
     {
