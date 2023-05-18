fe6e4f2a2d215326b793887dfb97456d7f26db98
botw0303 <zeldabotw0303@gmail.com>
2023-05-18 19:03:55 +0900

    Revert "Merge branch 'main' into buseong"
    This reverts commit 3c04ef587a77ec37009d509553fa1d4cdd8ab32e, reversing
    changes made to d0832c9b61e763c22b1e83e9f2b4fc3b16ed193a.

--- Client/Assets/01.Scripts/Player/PlayerMovement.cs
+++ Client/Assets/01.Scripts/Player/PlayerMovement.cs
@@ -1,68 +1,17 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private PlayerPropertySO _playerPropertySO;
-    
-    private CharacterController _characterController;
-    private PlayerInput _playerInput;
-
-    private Vector3 _movementVelocity;
-
-    public bool CanMove = true;
-
-    private void Awake()
-    {
-        _characterController = GetComponent<CharacterController>();
-        _playerInput = GetComponent<PlayerInput>();
-
-        _playerInput.OnMovementKeyPress += CalcAcceleration;
-        _playerInput.OnJumpKeyPress += Jump;
-        _playerInput.OnDuckingKeyPress += Ducking;
-    }
-
-    private void CalcAcceleration(Vector3 _inputVelocity)
-    {
-        _inputVelocity.Normalize();
-
-        
-        _movementVelocity = _inputVelocity * (_playerPropertySO.MoveSpeed * Time.fixedDeltaTime);
-        
-        // if(_movementVelocity.sqrMagnitude > 0)
-        // {
-        //     transform.rotation = Quaternion.LookRotation(_movementVelocity);
-        // }
-    }
-
-    private void StopMove()
-    {
-        _movementVelocity = Vector3.zero;
-    }
-    
-    private void FixedUpdate() 
-    {
-        if (CanMove)
-        {
-            _characterController.Move(_movementVelocity);
-        }
-    }
-
-    private void Move(Vector3 _directionInput)  
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
 
-    private void Jump()
-    {
-        
-    }
-
-    private void Ducking()
+    // Update is called once per frame
+    void Update()
     {
         
     }
