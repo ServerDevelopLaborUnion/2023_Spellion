d437d94cc6091da6fab7add96a3eea1a37db1cf4
SungJieun <sungjieun.dev@gmail.com>
2023-06-01 19:25:20 +0900

    ready to merge

---
+++ Client/Assets/01.Scripts/Interactive/RotateObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject
+{
+    
+    
+    IEnumerator RotateCoroutine(float time, float angle, bool clockWise = true)
+    {
+        Vector3 wise;
+        if (clockWise == true) wise = Vector3.up;
+        else wise = Vector3.down;
+        
+        
+    }
+}
